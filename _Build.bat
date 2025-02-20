8dcfd3d4ba93c6f70ea803027b1a49e4ad0120d9
TheAenema <thegreatnone1995@gmail.com>
2025-02-20 08:03:06 +0330

    Removal Phase B - Docs Removed

--- _Build.bat
+++ _Build.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 set PYTHONDONTWRITEBYTECODE=1
-scons platform=windows target=editor arch=x86_64 debug_symbols=true -j24
+scons platform=windows target=editor arch=x86_64 debug_symbols=false optimize=size -j24
 pause
